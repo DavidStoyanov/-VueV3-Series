d9671af65ff6764923bda749a0a7c6d9d5977c3e
David Stoyanov <david_st1@abv.bg>
2022-12-28 02:34:14 +0200

    commit lesson

--- blog/src/views/HomeView.vue
+++ blog/src/views/HomeView.vue
@@ -1,45 +1,43 @@
 <template>
   <div class="home">
-    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">click me!</button>
-    <button @click="name = 'Another One'">change name</button>
-    <button @click="age = 123">try change age</button>
-    <br>
-    <button @click="addAge(22)">Age Up</button>
-    <br>
-      <label>
-        <span>Name:</span>
-        <input v-model="name" type="text" />
-      </label>
+
+    <h1>Home</h1>
+
+    <div>
+      <h2>Refs</h2>
+      <p>My name is {{ nameRef.name }}</p>
+      <button @click="changeNameRef">Update name ref</button>
+    </div>
+
+    <div>
+      <h2>Reactive</h2>
+      <p>My name is {{ nameReactive.name }}</p>
+      <button @click="changeNameReactive">Update name reactive</button>
+    </div>
+
     <br>
   </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
+import { ref, reactive } from '@vue/reactivity'
 
 export default {
   name: 'HomeView',
   setup() {
-    let name = ref('mario')
-    let age = ref(30)
-    //let age = 30
+    let nameRef = ref({name: 'mario'})
+    let nameReactive = reactive({name: 'mario'})
 
-    const p = ref()
-
-    const handleClick = () => {
-      console.log(p, p.value)
+    const changeNameRef = () => {
+      nameRef.value.name = 'Another One'
     }
 
-    const changeName = () => {
-      name.value = 'Another One'
+    const changeNameReactive = () => {
+      // Doesn't work with primitive values/ only referent
+      nameReactive.name = 'Another One'
     }
 
-    const addAge = (value) => {
-      age.value + value;
-    }
-
-    return { name, age, handleClick, addAge, p }
+    return { nameRef, nameReactive, changeNameRef, changeNameReactive }
   }
 }
 </script>
